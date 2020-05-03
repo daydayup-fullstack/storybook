fb157cf83416f7e45e0538926b25e9d9e37f357f
lawrence415610 <lawrence415610@gmail.com>
2020-05-03 19:44:21 +1000

    finish the profile component(pm006), add some mock data and id generator(pm001) function in 3-Profile.stories.js

---
+++ src/stories/3-Profile.stories.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Profile from "../components/Profile/Profile";
+
+export default {
+    title: "Profile",
+    component: Profile,
+};
+
+
+/**
+ * unique id generator
+ * @param {Number} length 
+ */
+function getID(length) {
+    return Number(Math.random().toString().substr(3, length) + Date.now()).toString(36);
+}
+const length = 10;
+
+
+// mock avatars
+const avatars = [
+    'https://www.cartoonize.net/svgavatars/temp-avatars/svgA042783438729715684.png',
+    'https://www.cartoonize.net/svgavatars/temp-avatars/svgA4969441256135618.png',
+    'https://www.cartoonize.net/svgavatars/temp-avatars/svgA23287895074275755.png',
+    'https://www.cartoonize.net/svgavatars/temp-avatars/svgA4158187637132811.png',
+    'https://www.cartoonize.net/svgavatars/temp-avatars/svgA4283961938955121.png',
+    'https://www.cartoonize.net/svgavatars/temp-avatars/svgA9485385284121917.png',
+    'https://www.cartoonize.net/svgavatars/temp-avatars/svgA8781047972315394.png'
+]
+
+// mock user for normal test
+const user_1 = {
+    id: getID(length),
+    firstName: 'yelin',
+    lastName: 'liu',
+    avatar: avatars[4],
+    colorIndex: 1
+}
+
+// mock user for null test
+const user_2 = {
+    id: getID(length),
+    firstName: 'yelin',
+    lastName: 'liu',
+    avatar: null,
+    colorIndex: 6
+}
+
+
+export const WithAvatar = () => {
+    return <Profile user={user_1} />
+}
+
+export const WithoutAvatar = () => {
+    return <Profile user={user_2} />
+}
