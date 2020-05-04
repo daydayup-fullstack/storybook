1adec482ccba2a25eb65fd8d17d35e8821539107
Scott Wang <scotteau@gmail.com>
2020-05-04 13:27:00 +0800

    added sidemenu component

---
+++ src/components/SideMenu/SideMenu.js
@@ -0,0 +1,30 @@
+import "./SideMenu.css";
+import React, { useState } from "react";
+
+const SideMenu = () => {
+  const [shouldCollapse, setShouldCollapse] = useState(false);
+
+  return (
+    <div className="container">
+      <section className={"left"}>
+        <header>
+            <div>Logo</div>
+            <span className="material-icons" onClick={() => setShouldCollapse(true) }>menu_open</span>
+        </header>
+      </section>
+      <section className={`right ${shouldCollapse ? "collapse" : "expand"}`}>
+        <header>
+            <div className="title" onClick={() => setShouldCollapse(false)}>
+                {shouldCollapse && <span className={"material-icons icon"}>menu</span>}
+                <h2>Home</h2>
+            </div>
+
+          <div>Right</div>
+        </header>
+        <div className="content"></div>
+      </section>
+    </div>
+  );
+};
+
+export default SideMenu;
---
+++ src/stories/4-SideMenu.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SideMenu from "../components/SideMenu/SideMenu";
+
+export default {
+    title: "SideMenu",
+    component: SideMenu
+};
+
+
+export const Menu = () => {
+    return <SideMenu/>;
+};
