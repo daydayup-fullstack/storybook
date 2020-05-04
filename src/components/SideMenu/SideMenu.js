2fc7066c7df1fd62359fd78467ae4af320c62721
Scott Wang <scotteau@gmail.com>
2020-05-04 16:54:15 +0800

    cleanup

--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -4,27 +4,27 @@ import React, { useState } from "react";
 const SideMenu = () => {
   const [shouldCollapse, setShouldCollapse] = useState(false);
 
-  return (
-    <div className="container">
-      <section className={"left"}>
-        <header>
-            <div>Logo</div>
-            <span className="material-icons" onClick={() => setShouldCollapse(true) }>menu_open</span>
-        </header>
-      </section>
-      <section className={`right ${shouldCollapse ? "collapse" : "expand"}`}>
-        <header>
-            <div className="title" onClick={() => setShouldCollapse(false)}>
-                {shouldCollapse && <span className={"material-icons icon"}>menu</span>}
-                <h2>Home</h2>
-            </div>
+    return (
+        <div className="container">
+            <section className={"left"}>
+                <header>
+                    <div>Logo</div>
+                    <span className="material-icons" onClick={() => setShouldCollapse(true)}>menu_open</span>
+                </header>
+            </section>
+            <section className={`right ${shouldCollapse ? "collapse" : "expand"}`}>
+                <header>
+                    <div className="title" onClick={() => setShouldCollapse(false)}>
+                        {shouldCollapse && <span className={"material-icons icon"}>menu</span>}
+                        <h2>Home</h2>
+                    </div>
 
-          <div>Right</div>
-        </header>
-        <div className="content"></div>
-      </section>
-    </div>
-  );
+                    <div>Right</div>
+                </header>
+                <div className="content"/>
+            </section>
+        </div>
+    );
 };
 
 export default SideMenu;
