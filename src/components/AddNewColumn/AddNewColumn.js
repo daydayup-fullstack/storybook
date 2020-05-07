27a43b337eb0c82c3b30be8ded9c46447e048adf
Scott Wang <scotteau@gmail.com>
2020-05-07 21:54:35 +0800

    added title input section

--- src/components/AddNewColumn/AddNewColumn.js
+++ src/components/AddNewColumn/AddNewColumn.js
@@ -1,15 +1,48 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./AddNewColumn.css";
 
 const AddNewColumn = () => {
+  const [state, setState] = useState({ adding: false, currentValue: "" });
+  const inputElement = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+  };
+
+  const handleChange = (event) => {
+    setState({ ...state, currentValue: event.target.value });
+  };
+
+  const handleClick = () => {
+    state.adding && setState({ ...state, adding: false });
+  };
+
+  useEffect(() => {
+    if (inputElement.current) {
+      inputElement.current.focus();
+    }
+  }, [state.adding]);
+
   return (
-    <div className={"addNewColumn"}>
+    <div className={"addNewColumn"} onClick={handleClick}>
       <div className={"header"}>
-        <h2>
-          <span className={"material-icons"}>add</span>Add column
-        </h2>
-        <div className={"list"} />
-        {/*<input type="text" value={"Add Column"} />*/}
+        {!state.adding ? (
+          <h2 onClick={() => setState({ adding: true })}>
+            <span className={"material-icons"}>add</span>Add column
+          </h2>
+        ) : (
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={state.currentValue}
+              onChange={handleChange}
+              placeholder={"New Column"}
+              ref={inputElement}
+            />
+          </form>
+        )}
+        <div className={`list ${state.adding && "list--show"}`} />
       </div>
     </div>
   );
