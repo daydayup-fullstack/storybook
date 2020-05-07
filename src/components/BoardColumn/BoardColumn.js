2f35002b67276f5bc4748190284f36bc4236beed
Scott Wang <scotteau@gmail.com>
2020-05-07 11:08:29 +0800

    added beautiful-dnd to project and now the columns are draggable

--- src/components/BoardColumn/BoardColumn.js
+++ src/components/BoardColumn/BoardColumn.js
@@ -2,31 +2,41 @@ import React, { useState } from "react";
 import AddBoardTaskButton from "../AddBoardTaskButton/AddBoardTaskButton";
 import "./BoardColumn.css";
 import { action } from "@storybook/addon-actions";
+import { Draggable } from "react-beautiful-dnd";
 
-const BoardColumn = ({ column }) => {
+const BoardColumn = ({ column, index }) => {
   const [shouldHighlighted, setShouldHighlighted] = useState(false);
 
   return (
-    <div
-      className={`board-column ${shouldHighlighted && "board-column--hovered"}`}
-    >
-      <div
-        className="header"
-        onMouseEnter={() => setShouldHighlighted(true)}
-        onMouseLeave={() => setShouldHighlighted(false)}
-      >
-        <span className={"title"}>{column.title}</span>
-        <span
-          className={"material-icons button"}
-          onClick={action("more button clicked")}
+    <Draggable draggableId={column.id} index={index}>
+      {(provided) => (
+        <div
+          {...provided.draggableProps}
+          ref={provided.innerRef}
+          className={`board-column ${
+            shouldHighlighted && "board-column--hovered"
+          }`}
         >
-          more_horiz
-        </span>
-      </div>
-      <AddBoardTaskButton />
+          <div
+            className="header"
+            {...provided.dragHandleProps}
+            onMouseEnter={() => setShouldHighlighted(true)}
+            onMouseLeave={() => setShouldHighlighted(false)}
+          >
+            <span className={"title"}>{column.title}</span>
+            <span
+              className={"material-icons button"}
+              onClick={action("more button clicked")}
+            >
+              more_horiz
+            </span>
+          </div>
+          <AddBoardTaskButton />
 
-      <div className={"list"} />
-    </div>
+          <div className={"list"} />
+        </div>
+      )}
+    </Draggable>
   );
 };
 
--- src/components/KanbanBoard/KanbanBoard.js
+++ src/components/KanbanBoard/KanbanBoard.js
@@ -1,14 +1,34 @@
 import React from "react";
 import "./KanbanBoard.css";
 import BoardColumn from "../BoardColumn/BoardColumn";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 const KanbanBoard = ({ project }) => {
+  const onDragEnd = (result) => {
+    //    todo: reorder the columns
+  };
+
   return (
-    <div className={"kanban"}>
-      {project.columnOrder.map((columnId) => (
-        <BoardColumn column={project.columns[columnId]} />
-      ))}
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId={project.id} direction={"horizontal"}>
+        {(provided) => (
+          <div
+            className={"kanban"}
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {project.columnOrder.map((columnId, index) => (
+              <BoardColumn
+                key={columnId}
+                column={project.columns[columnId]}
+                index={index}
+              />
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
 
