7e28e73f30d7f410cfaa071dda3a0fa4cbbf8bc0
Scott Wang <scotteau@gmail.com>
2020-05-08 11:12:00 +0800

    added popup arrow & onclick methods

--- src/components/PopupTooltip/PopupTooltip.js
+++ src/components/PopupTooltip/PopupTooltip.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import "./PopupTooltip.css";
+import { action } from "@storybook/addon-actions";
 
 const PopupTooltip = () => {
-    return (
-        <div>
-            Hello from popuptooltip
-        </div>
-    )
-}
+  const [shouldShow, setShouldShow] = useState(false);
+
+  return (
+    <div className={"popup-tooltip"}>
+      <div className="content">
+        <ul>
+          <li onClick={action("add a new task")}>
+            <span className={"material-icons"}>check_circle_outline</span>
+            <span>Task</span>
+          </li>
+          <li onClick={action("add a new project")}>
+            <span className={"material-icons"}>list_alt</span>
+            <span>Project</span>
+          </li>
+
+          <li onClick={action("add a new person")}>
+            <span className={"material-icons"}>person_add</span>
+            <span>Invite</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default PopupTooltip;
