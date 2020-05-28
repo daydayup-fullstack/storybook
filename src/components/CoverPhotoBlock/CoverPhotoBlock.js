acf21d1e94035f46b199d3406080ba09c3bc9545
Scott Wang <scotteau@gmail.com>
2020-05-28 14:49:18 +0800

    renamed component UploadArea to Dropzone

--- src/components/CoverPhotoBlock/CoverPhotoBlock.js
+++ src/components/CoverPhotoBlock/CoverPhotoBlock.js
@@ -4,10 +4,7 @@ import { useCalculated } from "../DisplayPhotoBlock/DisplayPhotoBlock";
 
 const CoverPhotoBlock = () => {
   const imageUrl =
-    "https://www.logitechg.com/content/dam/gaming/en/products/pro-x-keyboard/pro-x-keyboard-hero.png";
-
-  // const imageUrl =
-  //   "https://media.wired.com/photos/5bd86f1cb0c71179a8e94cbd/16:9/w_1519,h_854,c_limit/macmini1.jpg";
+    "https://media.wired.com/photos/5bd86f1cb0c71179a8e94cbd/16:9/w_1519,h_854,c_limit/macmini1.jpg";
   const imageRef = React.useRef();
   const [height] = useCalculated("height", imageRef);
 
--- src/stories/27-Upload.stories.js
+++ src/stories/27-Upload.stories.js
@@ -3,6 +3,7 @@ import NewUploadBlock from "../components/NewUploadBlock/NewUploadBlcok";
 import DisplayPhotoBlock from "../components/DisplayPhotoBlock/DisplayPhotoBlock";
 import UploadingPhotoBlock from "../components/UploadingPhotoBlock/UploadingPhotoBlock";
 import CoverPhotoBlock from "../components/CoverPhotoBlock/CoverPhotoBlock";
+import Dropzone from "../components/Dropzone/Dropzone";
 
 export default {
   title: "Uploading & display",
@@ -16,3 +17,5 @@ export const displayUpload = () => <DisplayPhotoBlock />;
 export const uploadingPhotoBlock = () => <UploadingPhotoBlock />;
 
 export const coverPhotoBlock = () => <CoverPhotoBlock />;
+
+export const dropzone = () => <Dropzone />;
