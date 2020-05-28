d49c014fc3d0e8649e4bde98df1bc7c191495893
Scott Wang <scotteau@gmail.com>
2020-05-28 09:22:32 +0800

    added newUploadBlock component

---
+++ src/stories/27-Upload.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import NewUploadBlock from "../components/NewUploadBlock/NewUploadBlcok";
+
+export default {
+  title: "Uploading & display",
+  component: NewUploadBlock,
+};
+
+export const newUpload = () => <NewUploadBlock />;
