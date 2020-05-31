e1bdee0961da3b9430c242bbd3e4e0bdb40c5291
Scott Wang <scotteau@gmail.com>
2020-05-31 20:43:02 +0800

    now filterbar shows menu properly

---
+++ src/stories/29-FilterSort.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Filterbar from "../components/Filterbar/Filterbar";
+
+export default {
+  title: "Filter",
+  component: Filterbar,
+};
+
+export const taskList = () => <Filterbar />;
